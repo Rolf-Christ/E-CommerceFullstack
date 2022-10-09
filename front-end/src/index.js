5199bcc115f493c24e248b35b191aced67031fb1
Rolf-Christ <68683189+Rolf-Christ@users.noreply.github.com>
2022-10-09 18:01:37 +0100

    Premier commit du project .

---
+++ front-end/src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap'
+import 'jquery/dist/jquery.min.js'
+import './assets/js/script'
+import './assets/css/all.min.css'
+import './assets/css/style.css'
+import App from './App';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
